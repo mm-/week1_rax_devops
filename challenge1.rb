85e720b306558403925ac05de6eaae6404e57cf9
Masen Marshall <masen.marshall@rackspace.com>
2013-03-16 20:40:46 -0500

    Now we can build servers!

--- challenge1.rb
+++ challenge1.rb
@@ -3,6 +3,11 @@ require 'fog'
 # Set our custom fog credentials file:
 ENV['FOG_RC'] = '~/.rackspace_cloud_credentials'
 
+# Distro and size of the servers we're going to build
+# In this case centos 6.3 on 512MB servers
+image_id = 'c195ef3b-9195-4474-b6f7-16e5bd86acd0'
+flavor_id = '2'
+
 # Name of the servers, will be iterated ie: web1, web2, web3
 server_label = 'web'
 
@@ -24,7 +29,9 @@ server_limit = 3
 until suffix > server_limit
 	server_name = server_label+suffix.to_s
 
-	puts server_name
+	server = connection.servers.create(:name => server_name, :flavor_id => flavor_id, :image_id => image_id)
+	
+	puts "Creating"+server_name
 
 	suffix +=1
 end
