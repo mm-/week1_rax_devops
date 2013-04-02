9d3384f7e4daf953b5f0ac123fd81e0747676691
Masen Marshall <masen.marshall@rackspace.com>
2013-04-02 14:46:40 -0500

    Added comments and a TODO

--- challenge4.rb
+++ challenge4.rb
@@ -4,6 +4,8 @@ require 'trollop'
 
 ENV['FOG_RC'] = '~/.rackspace_cloud_credentials'
 
+
+# Accept --ip and --fqdn as arguments
 opts = Trollop::options do
   opt :ip, "IP address for A record", :type => :string
   opt :fqdn, "FQDN to add the record for", :type => :string
@@ -14,21 +16,23 @@ ip = opts[:ip]
 fqdn = opts[:fqdn]
 
 
+#TODO Make these arguments mandatory so the script won't run without them
 
 
+# Connect to RSC DNS
 dns = Fog::DNS.new({
     :provider => 'Rackspace',
     #:rackspace_email => 'mithofrose@gmail.com',
                  })
 
 
-
+# Create the zone
 zone = dns.zones.create(
     :domain => "#{fqdn}",
     :email => 'lol@topseller.org'
 )
 
-
+# Create the record
 record = zone.records.create(
     :value => "#{ip}",
     :name => "#{fqdn}",
